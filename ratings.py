bf28f7c5bc2be7fc42c690c650de677defcee60e
Hackbright Student <no-reply@hackbrightacademy.com>
2018-09-10 16:22:29 -0700

    allowing the user to add ratings

--- ratings.py
+++ ratings.py
@@ -13,5 +13,17 @@ for line in my_file:
     words_list = (line.rstrip()).split(':')
     restaurants_dict[words_list[0]] = words_list[1]
 
+while True:
+    choice =input("would you like to add your ratings? Y/N ")
+    if choice.upper() == "N":
+        break
+
+    restaurant = input("Please enter the restaurant name: ")
+    rating = input("Please enter it's rating: ")
+    restaurants_dict[restaurant.title()] = rating
+
+
+
 for restaurant, rating in sorted(restaurants_dict.items()):
-    print("{} is rated at {}." .format(restaurant, rating))
+    print("{} is rated at {}." .format(restaurant, int(rating)))
+
